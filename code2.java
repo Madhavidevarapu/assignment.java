57dc417997c96249d4f72252856f242a0554e0a0
Madhavidevarapu <140319997+Madhavidevarapu@users.noreply.github.com>
2024-03-12 14:00:10 +0530

    Create code2.java

---
+++ code2.java
@@ -0,0 +1,26 @@
+public class FindMaxElement {
+
+    public static void main(String[] args) {
+        int[] array = {7, 12, 4, 9, 5, 18, 10};
+
+       
+        int maxElement = findMaxElement(array);
+
+        System.out.println("Array: " + Arrays.toString(array));
+        System.out.println("Maximum Element: " + maxElement);
+    }
+
+    private static int findMaxElement(int[] array) {
+        if (array.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        int maxElement = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxElement) {
+                maxElement = array[i];
+            }
+        }
+        return maxElement;
+    }
+}
